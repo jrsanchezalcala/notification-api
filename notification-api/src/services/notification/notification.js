dffdf0bac7b51ed09404c639d652e5e81e6a07f2
Juan Sanchez <ap@debian10.juan>
2023-03-15 23:19:04 +0100

    Add cliend and sender api key authorization

--- notification-api/src/services/notification/notification.js
+++ notification-api/src/services/notification/notification.js
@@ -1,6 +1,7 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/service.html
 
 import { hooks as schemaHooks } from '@feathersjs/schema'
+import { NotAuthenticated } from '@feathersjs/errors'
 import {
   notificationDataValidator,
   notificationQueryValidator,
@@ -14,9 +15,12 @@ import { notificationPath, notificationMethods } from './notification.shared.js'
 
 export * from './notification.class.js'
 export * from './notification.schema.js'
-
+var CLIENT_API_KEYS, SENDER_API_KEYS
 // A configure function that registers the service and its hooks via `app.configure`
 export const notification = (app) => {
+  //set valid api-keys on memory
+  CLIENT_API_KEYS = app.get('CLIENT_API_KEYS')
+  SENDER_API_KEYS = app.get('SENDER_API_KEYS')
   // Register our service on the Feathers application
   app.use(notificationPath, new NotificationService(getOptions(app)), {
     // A list of all methods this service exposes externally
@@ -37,9 +41,10 @@ export const notification = (app) => {
         schemaHooks.validateQuery(notificationQueryValidator),
         schemaHooks.resolveQuery(notificationQueryResolver)
       ],
-      find: [],
-      get: [],
+      find: [checkClientAPIKey],
+      get: [checkClientAPIKey],
       create: [
+        checkSenderAPIKey,
         schemaHooks.validateData(notificationDataValidator),
         schemaHooks.resolveData(notificationDataResolver)
       ],
@@ -55,6 +60,23 @@ export const notification = (app) => {
   })
 
   function notAvailable() {
-    throw new Error('NOT AVAILABLE')
+    throw new NotAuthenticated('NOT AVAILABLE')
+  }
+
+  function checkClientAPIKey(context, next) {
+    const authHeader = context.params.headers.authorization
+    if (authHeader && (CLIENT_API_KEYS.includes(authHeader) || checkSenderAPIKey(context, next))) {
+      return true
+    } else {
+      throw new NotAuthenticated('Not Allowed')
+    }
+  }
+  function checkSenderAPIKey(context, next) {
+    const authHeader = context.params.headers.authorization
+    if (authHeader && SENDER_API_KEYS.includes(authHeader)) {
+      return true
+    } else {
+      throw new NotAuthenticated('Not Allowed')
+    }
   }
 }
